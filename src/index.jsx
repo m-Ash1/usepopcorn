4a25dc922556e9d0cebd2e3e2f0762f6cd07222d
Mahmoud Ashraf <mashraffarouk2702@gmail.com>
2023-08-23 03:58:23 +0300

    some initial things

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './App.jsx'
+// import './index.css'
+import StarRating from "./StarRating.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <StarRating maxRating={10}/>
+  </React.StrictMode>
+);
